d7f7ffad60b48bf39329cb452b279651b72b9e29
Yagnitha <98573889+Yagnitha18@users.noreply.github.com>
2024-10-02 16:48:45 +0530

    Create Java

---
+++ EXERCISE 2/ Virtual Classroom Manager/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private String id;
+
+    public Student(String studentId) {
+        this.id = studentId;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
